2f7daf6bbb596891317e83ae2a4f812e13a0d5ec
dhushordhrubo <dhushordhrubo@gmail.com>
2021-07-15 12:28:28 +0600

    'final_code'

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -21,6 +21,13 @@ contract Decentragram {
     uint tipAmount,
     address payable author
   );
+   event ImageTipped(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
 
   // Create Image
   function uploadImage(string memory _imgHash,string memory _description) public{
@@ -37,5 +44,21 @@ contract Decentragram {
     // Trigger an event
     emit ImageCreated(imageCount,_imgHash,_description,0,msg.sender);
   }
-  // Tip Image
+  // Tip Images
+  function tipImageOwner(uint _id) public payable{
+    // Make sure the id is valid
+    require(_id > 0 && _id <= imageCount);
+    // Fetch the image
+    Image memory _image = images[_id];
+    // Fetch the author
+    address payable _author = _image.author;
+    // Pay the author by sending them Ether.
+    address(_author).transfer(msg.value);
+    // Increment the tip amount
+    _image.tipAmount = _image.tipAmount + msg.value;
+    // Update the image
+    images[_id] = _image;
+    // Trigger an Event
+    emit ImageTipped(_id,_image.hash,_image.description,_image.tipAmount,_author);
+  }
 }
